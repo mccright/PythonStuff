8f543a4c7b2b93b04c32a1b1beb0e170dead2027
mccright <6339517+mccright@users.noreply.github.com>
2021-08-07 20:25:02 -0500

    Update getSomeIPInfo.py

--- getSomeIPInfo.py
+++ getSomeIPInfo.py
@@ -8,13 +8,14 @@ from time import sleep
 # This is an idiom from ipinfo and in example code all over the Internet.
 
 def main():
-    os.system('cls' if os.name=='nt' else 'clear')
+    #os.system('cls' if os.name == 'nt' else 'clear')
     try:
-        ipaddress=input("ip address: ")
+        ipaddress = input("ip address: ")
         sleep(1)
-        url="https://ipinfo.io/{}".format(ipaddress)
-        response=requests.get(url)
+        url = "https://ipinfo.io/{}".format(ipaddress)
+        response = requests.get(url)
         jsonstr=json.loads(response.text)
+        print("IP\t\t: {}".format(jsonstr['ip']))
         print("Hostname\t: {}".format(jsonstr['hostname']))
         print("City\t\t: {}".format(jsonstr['city']))
         print("Region\t\t: {}".format(jsonstr['region']))
@@ -24,8 +25,26 @@ def main():
         print("Organization\t: {}".format(jsonstr['org']))
         print("Location\t: {}".format(jsonstr['loc']))
     except Exception as e:
-        print("Failed this run. Error: {} -- {}".format(e,(sys.exc_info())))
+        print("Failed this run. Error: {} -- {}".format(e, (sys.exc_info())))
+
+
+def main_iterate():
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    try:
+        ipaddress = input("ip address: ")
+        sleep(1)
+        url = "https://ipinfo.io/{}".format(ipaddress)
+        response = requests.get(url)
+        json_str = json.loads(response.text)
+        for key in json_str:
+            print(key, ' = ', json_str[key])
+    except Exception as e:
+        print("Failed this run. Error: {} -- {}".format(e, (sys.exc_info())))
         exit()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    print("First, hard code specific key/value pairs.")
     main()
+    print("Now, iterate through every key/value pair.")
+    main_iterate()
