7c39f6bf246397764e66edbf0c5e457258ded7cf
Matt McCright <6339517+mccright@users.noreply.github.com>
2021-12-27 20:13:27 -0600

    added most-simple-api-post.py
    as a spare skeleton

---
+++ most-simple-api-post.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# NOTE: Model code for a one-off simplified http post to a simple API.
+#       The most simple idiom of this category.
+#       If used repeatedly, implement inputs and error handling.
+#       Some APIs may also need to deal with paging of response data.
+# USAGE: Copy and morph for your intended use case
+
+import json
+import requests
+
+FIRST_VARIABLE = "" # Replace with what is needed for var 1
+SECOND_VARIABLE = "" # Replace with what is needed for var 2
+
+def do_the_thing():
+    # Causes the action, gets the data, etc.
+    # Documentation: https://the.documentation.host/dir/docfile
+
+    header = { # Modify as needed
+        "Content-Type": "application/json"
+    }
+
+    payload = { # Modify as needed
+        "first_placeholder": FIRST_VARIABLE, 
+        "second_placeholder": SECOND_VARIABLE 
+    }
+
+    response = requests.post('https://targetHost.targetDomain.dom/the/api', 
+                            data=json.dumps(payload),
+                            headers=header)
+	
+    if response.json()["status"] == "success":
+        print('Something relevant ')
+    else:
+        print(response.text) # print message if unsuccessful, might need to ASCIIfy it first
+
+if __name__ == '__main__':
+    do_the_thing()
