cddb0937689b48d3400af7d1ad0a1f66ff5e4d18
Matt McCright <6339517+mccright@users.noreply.github.com>
2022-01-29 12:01:19 -0600

    update getSomeIPInfo.py
    changed how to ingest json response content

--- getSomeIPInfo.py
+++ getSomeIPInfo.py
@@ -15,7 +15,7 @@ def main():
         sleep(1)
         url = "https://ipinfo.io/{}".format(ipaddress)
         response = requests.get(url)
-        jsonstr=json.loads(response.text)
+        jsonstr = decode_json(response.text)
         print("IP\t\t: {}".format(jsonstr['ip']))
         print("Hostname\t: {}".format(jsonstr['hostname']))
         print("City\t\t: {}".format(jsonstr['city']))
@@ -36,12 +36,16 @@ def main_iterate():
         sleep(1)
         url = "https://ipinfo.io/{}".format(ipaddress)
         response = requests.get(url)
-        json_str = json.loads(response.text)
+        json_str = decode_json(response.text)
         for key in json_str:
             print(key, ' = ', json_str[key])
     except Exception as e:
         print("Failed this run. Error: {} -- {}".format(e, (sys.exc_info())))
         exit()
+
+
+def decode_json(json_input):
+    return json.loads(json_input)
 
 
 if __name__ == "__main__":
