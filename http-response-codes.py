f0a256520aa7095022e54f4ba5a093ee6ecbef9f
mccright <6339517+mccright@users.noreply.github.com>
2022-12-25 06:52:58 -0600

    Update http-response-codes.py
    Migrated from Python 3.6 to 3.10

--- http-response-codes.py
+++ http-response-codes.py
@@ -3,8 +3,13 @@
 import sys
 from http import HTTPStatus
 
+# Python 3.7 will stop getting security updates in June 2023. 
+# 3.8, 3.9, 3.10, and 3.11 are available and you should
+# upgrade if you are using 3.7 or below.
+
+
 MIN_PYTHON_MAJOR_VERSION = 3
-MIN_PYTHON_MINOR_VERSION = 6
+MIN_PYTHON_MINOR_VERSION = 10
 
 # Check for Python 3 to support our usage of the print statement
 if sys.version_info < (MIN_PYTHON_MAJOR_VERSION, MIN_PYTHON_MINOR_VERSION):
