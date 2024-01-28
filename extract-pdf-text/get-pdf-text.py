019e145f1cdf2035750e980f6d7fd34599893cb1
mccright <6339517+mccright@users.noreply.github.com>
2024-01-28 14:13:26 -0600

    Update get-pdf-text.py
    Updated imports to align with my "conventions:" https://github.com/mccright/PythonStuff/tree/main#python-conventions
    
    Made the "separator" function start each line with a comment (#) character.
    
    Plus a little, minor cleanup.

--- extract-pdf-text/get-pdf-text.py
+++ extract-pdf-text/get-pdf-text.py
@@ -1,22 +1,20 @@
 # This approach to using 'pypdf' to extract text from PDF files 
-# often works well for me. It is from the pypdf docs. 
+# often works well-enough for me. It is from the pypdf docs. 
 # https://pypdf.readthedocs.io/en/stable/user/extract-text.html
 # pypdf can extract a range of PDF components: 
 # https://pypdf2.readthedocs.io/en/3.0.0/_modules/PyPDF2/_reader.html
 # pypdf errors are documented in:  
 # https://github.com/py-pdf/pypdf/blob/main/pypdf/errors.py
 # 
-# ToDo: Add some text cleanup via post-processing.
+# As needed: Add some text cleanup via post-processing.
 # The link below lists a few examples of what can be done as well 
 # as a community recipie that can be used as a best-practice 
 # general purpose post processing step.
 # https://pypdf.readthedocs.io/en/stable/user/post-processing-in-text-extraction.html
-from pypdf import PdfReader
+import pypdf
 import os
 import sys
 import time
-
-separator = "= = = = = = = = = = = = = = = = = = = = = = = ="
 
 
 def openpdf(filename: str) -> bytes:
@@ -24,7 +22,7 @@ def openpdf(filename: str) -> bytes:
         if not os.path.exists(filename):
             raise Exception(f"File path \"{filename}\" does not \
 appear to exist, or there may be a permission issue.")
-        pdfreader = PdfReader(filename)
+        pdfreader = pypdf.PdfReader(filename)
     except Exception as e:
         print(f"Problem with PdfReader. Did you specify a valid filename? {e}")
         sys.exit()
@@ -85,9 +83,10 @@ def current_local_time() -> str:
 
 
 def print_separator(msgstring: str):
-    print(f"{separator}")
-    print(f"{msgstring}")
-    print(f"{separator}")
+    separator = "= = = = = = = = = = = = = = = = = = = = = = = ="
+    print(f"# {separator}")
+    print(f"# {msgstring}")
+    print(f"# {separator}")
 
 
 def main():
@@ -98,11 +97,11 @@ def main():
     filename = sys.argv[1]
     if filename != None:
         reader = openpdf(filename)
-        print_separator(f"Start {filename} text.\r\nExtracted on: {current_local_time()}")
+        print_separator(f"Start \"{filename}\" text.\r\n# Extracted on: {current_local_time()}")
         extracted_text = get_text_from_pdfreader(reader)
         # Add any post-processing functions here 
         print(f"{replace_ligatures(extracted_text)}")
-        print_separator(f"End {filename} text.\r\nExtracted on: {current_local_time()}")
+        print_separator(f"End \"{filename}\" text.\r\n# Extracted on: {current_local_time()}")
         sys.exit()
 
 
