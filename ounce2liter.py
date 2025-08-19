e1434533bbc8f11240d02f1279a02fab2a8be4e3
mccright <6339517+mccright@users.noreply.github.com>
2025-08-19 10:42:47 -0500

    Simplified liter/ounce conversion scripts
    Simplified liter/ounce conversion scripts

--- ounce2liter.py
+++ ounce2liter.py
@@ -7,10 +7,10 @@ import sys
 # Liters = ounces / 33.814
 # Ounces = liters * 33.814
 
-def l2o(f_value):
-    liters = float(f_value)
-    ounces = (liters * 33.814)
-    return ounces
+def o2l(f_value):
+    ounces = float(f_value)
+    liters = (ounces / 33.814)
+    return liters
 
 
 def main(num_args: int, usage: str):
@@ -20,10 +20,10 @@ def main(num_args: int, usage: str):
         print(usage_message)
         sys.exit(1)
     else:
-        print(f"{l2o(sys.argv[1]):.2f}")
+        print(f"{o2l(sys.argv[1]):.2f}")
 
 
 if __name__ == '__main__':
-    inputs_4_usage_msg = '<volume_in_liters>'
+    inputs_4_usage_msg = '<volume_in_ounces>'
     usage_msg = f"USAGE: python3 script_name {inputs_4_usage_msg}"
     main(2, usage_msg)
